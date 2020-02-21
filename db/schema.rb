584ca3ad0e91282de28d238debb4f29682070adb
ShirasuSalaD <hihihipoke@gmail.com>
2020-02-21 18:47:16 +0900

    add: user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_02_003108) do
+ActiveRecord::Schema.define(version: 2020_02_21_090608) do
 
   create_table "fixedcost_values", force: :cascade do |t|
     t.integer "fixedcost_id", null: false
@@ -42,6 +42,15 @@ ActiveRecord::Schema.define(version: 2019_05_02_003108) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "user_id", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_users_on_user_id", unique: true
   end
 
   create_table "variablecost_values", force: :cascade do |t|
