6cdfdf56d29fedd0d2f971f5a13fac627bfa1c3b
ShirasuSalaD <hihihipoke@gmail.com>
2020-02-23 16:54:03 +0900

    add: bootstrap navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,7 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery
+//= require popper
+//= require bootstrap
 //= require_tree .
-//= require serviceworker-companion
